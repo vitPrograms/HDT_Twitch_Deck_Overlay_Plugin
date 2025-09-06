84b864705277046f1ee1931a989b77d007070322
vitPrograms <vitalii.beley@gmail.com>
2025-09-06 19:47:03 +0300

    v1.1.2

--- Plugin.cs
+++ Plugin.cs
@@ -25,7 +25,7 @@ namespace TwitchDeckOverlay
         public string Description => "Displays decks shared in a Twitch chat";
         public string ButtonText => "Settings";
         public string Author => "Proogro";
-        public Version Version => new Version(1, 1, 1);
+        public Version Version => new Version(1, 1, 2);
         public MenuItem MenuItem => _menuItem;
 
         public Plugin()
